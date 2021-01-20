3f42346943d874572f3e59b1fc9ffce0e8884ea6
arupsarkar-sfdc <arup.sarkar@salesforce.com>
2021-01-20 15:40:09 -0500

    Added orgInit.sh file

---
+++ orgInit.sh
@@ -0,0 +1,11 @@
+# Create the scratch org (uncomment for the SFDX Deployer)
+sfdx shane:org:create -f config/project-scratch-def.json -d 7 -s -n --userprefix admin
+
+# Push the metadata into the new scratch org.
+sfdx force:source:push
+
+# Set the default password.
+sfdx shane:user:password:set -g User -l User -p sfdx1234
+
+# Open the demo org.
+sfdx force:org:open
