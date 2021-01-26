c029c25ff8d69c0d1f8a2cd6f4c3c01705f692cb
arupsarkar-sfdc <arup.sarkar@salesforce.com>
2021-01-25 23:52:54 -0500

    Added import statement in orginit script

--- orgInit.sh
+++ orgInit.sh
@@ -1,11 +1,10 @@
 # Create the scratch org (uncomment for the SFDX Deployer)
 sfdx shane:org:create -f config/project-scratch-def.json -d 7 -s -n --userprefix admin --userdomain sfdc.ninjas
-
 # Push the metadata into the new scratch org.
 sfdx force:source:push
-
+# Import data in the scratch orgs
+sfdx force:data:tree:import -p data/Account-Contact.json
 # Set the default password.
 sfdx shane:user:password:set -g User -l User -p sfdx1234
-
 # Open the demo org.
 sfdx force:org:open
